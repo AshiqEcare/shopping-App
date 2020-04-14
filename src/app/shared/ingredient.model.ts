8639c14336655e465698a1cc32d275acf1ac6b3e
ashiq.ansari <ashiq.ansari@novacisdigital.com>
2020-04-14 14:51:20 +0530

    EOD_14-04-20

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,14 @@
+export class Ingredient {
+  // OLd Method
+
+  // public name: string;
+  // public amount: number;
+
+  // constructor(name:string, amount:number) {
+  //   this.name = name;
+  //   this.amount = amount;
+  // }
+
+  // New Method
+  constructor(public name: string, public amount: number) {}
+}
